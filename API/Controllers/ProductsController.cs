bef606f82186ef25abab0526caf80926a5ab4bac
khalifeh Ahmad <khalifeh92ahmed@hotmail.com>
2023-12-24 08:40:56 -0800

    refactoring and configuration

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using Infrastructure.Data;
 using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 
 namespace API.Controllers
@@ -13,20 +14,21 @@ namespace API.Controllers
   [Route("api/[controller]")]
   public class ProductsController:ControllerBase
   {
-    private readonly StoreContext _context;
-    public ProductsController(StoreContext context){
-      _context = context;
+
+    private readonly IProductRepository _repo;
+    public ProductsController(IProductRepository repo){
+      _repo = repo;
     }
     [HttpGet]
     public async Task<ActionResult<List<Product>>> GetProducts()
     {
-      var prdcts = await _context.Products.ToListAsync();
+      var prdcts = await _repo.GetProductsAsync();
       return Ok(prdcts);
     }
     [HttpGet("{id}")]
     public async Task<ActionResult<Product>> GetProduct(int id)
     {
-      return await _context.Products.FindAsync(id);
+      return await _repo.GetProductByIdAsync(id);
     }
   }
 }
