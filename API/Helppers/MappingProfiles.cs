d7b8bda33dd577d683e69191e7346ce0c19b365e
khalifeh Ahmad <khalifeh92ahmed@hotmail.com>
2023-12-29 14:55:07 -0800

    deal with Api images

--- API/Helppers/MappingProfiles.cs
+++ API/Helppers/MappingProfiles.cs
@@ -10,8 +10,8 @@ namespace API.Helppers
         {
             CreateMap<Product, ProductToReturnDto>()
                 .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
-                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name));
-        
+                .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+                .ForMember(d => d.ImgUrl, o => o.MapFrom<ProductUrlResolver>());
         }
   }
 }
---
+++ API/Helppers/ProductUrlResolver.cs
@@ -0,0 +1,25 @@
+using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace API.Helppers
+{
+  public class ProductUrlResolver : IValueResolver<Product, ProductToReturnDto, string>
+  {
+
+    private  readonly IConfiguration _conf;
+    public ProductUrlResolver(IConfiguration conf)
+    {
+      _conf = conf;
+    }
+    public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+    {
+       if (!string.IsNullOrEmpty(source.ImgUrl))
+            {
+                return _conf["ApiUrl"] + source.ImgUrl;
+            }
+            return null;
+    }
+  }
+}
